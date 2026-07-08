bce631734aaef922f8e118f7daf2039eb512bfa6
carl567 <carl@gg-server.local>
2026-07-08 21:50:02 +0800

    chore: vite代理地址 localhost → 127.0.0.1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     proxy: {
-      '/api': 'http://localhost:5001',
+      '/api': 'http://127.0.0.1:5001',
     }
   },
   build: {
